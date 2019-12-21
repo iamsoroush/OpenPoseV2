fb42098bbeea86dd9fa1d2e625743169f2f6a28a
Soroush <soroush.moazed@gmail.com>
2019-12-21 10:37:07 +0330

    initial commit

--- config.py
+++ config.py
@@ -1,9 +1,24 @@
 import os.path.join as osjoin
 from os.path import abspath, normpath
 
-class Config:
+
+class ModelConfig:
 
     def __init__(self):
+        self.joint_threshold = 0.1
+        self.connection_threshold = 0.05
+        self.tf_resize_method = 'bicubic'
+        self.openpose_weights_path = osjoin('models', 'openpose_body25_keras.h5')
+        self.openpose_config_path = osjoin('models', 'config')
+        self.input_res = 228
+        self.use_gaussian_filtering = True
+        self.gaussian_kernel_sigma = 3
 
-        self.openpose_weights_path = join('models', 'openpose_body25_keras.h5')
-        self.openpose_config_path =
+
+class HyperConfig:
+
+    def __init__(self):
+        self.use_gpu = True
+        self.gpu_device_number = 0
+        self.scale_search = (0.8, 1.0, 1.2)
+        self.pad_value = 128
