3a7d1b42d736981699c2d87247063466913e612a
Soroush <soroush.moazed@gmail.com>
2019-12-29 13:31:06 +0330

    Detection class added and Person modified.

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,69 @@
 import numpy as np
 
 
+class Person:
+
+    """Base class for tracking trajectories.
+
+    Access to trjectory using self.detections.
+
+    :param detections: dict('frame_indx': Detection)"""
+
+    def __init__(self, p_id):
+        self.id = p_id
+        self.detections = dict()
+
+    def update(self, detection, frame_indx):
+        assert isinstance(detection, Detection), 'detection must be an object of Detection class.'
+        self.detections[frame_indx] = detection
+        detection.id = self.id
+
+
+class Detection:
+
+    def __init__(self,
+                 key_points,
+                 transformed_candidate,
+                 person_subset,
+                 confidence,
+                 bbox,
+                 pose_features):
+        self.key_points = key_points
+        self.transformed_candidate = transformed_candidate
+        self.person_subset = person_subset
+        self.confidence = confidence
+        self.bbox = bbox
+        self.pose_features = pose_features
+        self.pose_error = None
+        self.id = None
+        self.colors = [[255, 0, 0], [255, 85, 0], [255, 170, 0],
+                       [255, 255, 0], [170, 255, 0], [85, 255, 0],
+                       [0, 255, 0], [0, 255, 85], [0, 255, 170],
+                       [0, 255, 255], [0, 170, 255], [0, 85, 255],
+                       [0, 0, 255], [85, 0, 255], [170, 0, 255],
+                       [255, 0, 255], [255, 0, 170], [255, 0, 85],
+                       [255, 170, 85], [255, 170, 170], [255, 170, 255],
+                       [255, 85, 85], [255, 85, 170], [255, 85, 255],
+                       [170, 170, 170]]
+
+    def get_crop(self, img):
+        """:arg img: RGB(0, 255) image."""
+
+        person = img[self.bbox.y_up: self.bbox.y_up + self.bbox.height,
+                     self.bbox.x_left: self.bbox.x_left + self.bbox.width,
+                     :]
+        return person
+
+    def get_bbox_color(self):
+        if self.id is None:
+            return 0, 255, 0
+        else:
+            return self.colors[int(self.id)]
+
+    def calc_error(self, ref_features):
+        pass
+
+
 class BoundingBox:
 
     def __init__(self, x_min, x_max, y_min, y_max):
@@ -8,23 +71,7 @@ class BoundingBox:
         self.y_up = y_min
         self.width = x_max - x_min
         self.height = y_max - y_min
-
-
-class Person:
-
-    def __init__(self, keypoints, confidence, bbox, pose_features):
-        self.keypoints = keypoints
-        self.confidence = confidence
-        self.bbox = bbox
-        self.pose_features = pose_features
-
-    def get_person_data(self, img):
-        """:arg img: RGB(0, 255) image."""
-
-        person = img[self.bbox.y_up: self.bbox.y_up + self.bbox.height,
-                     self.bbox.x_left: self.bbox.x_left + self.bbox.width,
-                     :]
-        return person
+        self.data = [x_min, y_min, x_max, y_max]
 
 
 class FeatureExtractor:
