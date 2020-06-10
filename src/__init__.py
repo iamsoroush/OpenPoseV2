cace4b552a62282c65b421f0bf048dcfcfc54a45
Soroush <soroush.moazed@gmail.com>
2020-06-10 14:36:32 +0430

    added PROJECT_ROOT as global variable

--- src/__init__.py
+++ src/__init__.py
@@ -1,3 +1,8 @@
+from os.path import normpath, abspath, dirname, realpath
+
 from .aida import AIDA
 from .models import OpenPoseV2
 from .config import OpenPoseV2Config, HyperConfig, PoseCorrectionConfig
+
+
+PROJECT_ROOT = normpath(abspath(dirname(dirname(realpath(__file__)))))
