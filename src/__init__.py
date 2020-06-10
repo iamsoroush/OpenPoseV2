8cd51743bf0b9b5fb4ca9818a9565dbf35c31fab
Soroush <soroush.moazed@gmail.com>
2020-06-10 14:43:46 +0430

    syntax

--- src/__init__.py
+++ src/__init__.py
@@ -1,8 +1,6 @@
 from os.path import normpath, abspath, dirname, realpath
+PROJECT_ROOT = normpath(abspath(dirname(dirname(realpath(__file__)))))
 
 from .aida import AIDA
 from .models import OpenPoseV2
 from .config import OpenPoseV2Config, HyperConfig, PoseCorrectionConfig
-
-
-PROJECT_ROOT = normpath(abspath(dirname(dirname(realpath(__file__)))))
