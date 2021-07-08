40688f238c77827c87f08bc8a02feb8612dc7ad1
soroush <soroush.moazed@gmail.com>
2021-07-08 14:54:21 +0430

    some code clean-up

--- src/__init__.py
+++ src/__init__.py
@@ -3,4 +3,4 @@ PROJECT_ROOT = normpath(abspath(dirname(dirname(realpath(__file__)))))
 
 from .detectracker import DetecTracker
 from .models import OpenPoseV2
-from .config import OpenPoseV2Config, HyperConfig, PoseCorrectionConfig
+from .config import OpenPoseV2Config, HyperConfig
